97bf8229e0f3439579d644dbc99edddd35dbd967
kinderhead <kinderhead512@gmail.com>
2022-07-24 10:15:18 -0400

    work

---
+++ T2L/AST/ASTStatements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2L.AST
+{
+    public class ASTStatements : IAST
+    {
+        public readonly List<IAST> Statements = new();
+        
+        public ASTStatements(List<IAST> statements)
+        {
+            Statements = statements;
+        }
+    }
+}
--- T2L/Parser.cs
+++ T2L/Parser.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using sly.parser.generator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using T2L.AST;
 
 namespace T2L
 {
     public class Parser
     {
+        [Production("start: statements*")]
+        public IAST Start(List<IAST> statements)
+        {
+            return new ASTStatements(statements);
+        }
     }
 }
