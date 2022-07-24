dc7728c7015bb1ce5c2239691b85e5c3388533f3
kinderhead <kinderhead512@gmail.com>
2022-07-24 07:33:19 -0400

    stuff

---
+++ T2L/Compiler.cs
@@ -0,0 +1,26 @@
+﻿using sly.parser.generator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T2L.AST;
+
+namespace T2L
+{
+    public class Compiler
+    {
+        public static void Compile(string code)
+        {
+            Parser raw_parser = new();
+            ParserBuilder<Lexer, IAST> builder = new();
+            var parser = builder.BuildParser(raw_parser, ParserType.EBNF_LL_RECURSIVE_DESCENT, "start").Result;
+
+            var result = parser.Parse(code);
+            if (result.IsError)
+            {
+                result.Errors.ForEach(error => Console.WriteLine($"{error.ErrorType}: {error.ErrorMessage}"));
+            }
+        }
+    }
+}
---
+++ T2L/Lexer.cs
@@ -0,0 +1,85 @@
+﻿using sly.lexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2L
+{
+    public enum Lexer
+    {
+        [Keyword("if")]
+        IF,
+
+        [Double]
+        NUMBER,
+
+        [AlphaNumDashId]
+        ID,
+
+        [String("\"", "\\")]
+        [String("'", "\\")]
+        DOUBLE_STRING,
+
+        [Comment("//", "/*", "*/")]
+        COMMENT,
+
+        [Sugar("(")]
+        LPAREN,
+
+        [Sugar(")")]
+        RPAREN,
+
+        [Sugar("{")]
+        LCBRACKET,
+
+        [Sugar("}")]
+        RCBRACKET,
+
+        [Sugar("[")]
+        LSBRACKET,
+
+        [Sugar("]")]
+        RSBRACKET,
+
+        [Sugar("+")]
+        PLUS,
+
+        [Sugar("-")]
+        MINUS,
+
+        [Sugar("*")]
+        MUL,
+
+        [Sugar("/")]
+        DIV,
+
+        [Sugar("%")]
+        MOD,
+
+        [Sugar("=")]
+        ASSIGN,
+
+        [Sugar("==")]
+        EQ,
+
+        [Sugar("!=")]
+        NEQ,
+
+        [Sugar("<")]
+        LT,
+
+        [Sugar(">")]
+        GT,
+
+        [Sugar("<=")]
+        LTE,
+
+        [Sugar(">=")]
+        GTE,
+
+        [Sugar(";")]
+        SEMICOLON,
+    }
+}
